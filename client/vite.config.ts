1511e0236bd04cbf77b7dea7306198825db00323
Egor Gavrilenko <bambloo769@gmail.com>
2023-11-26 21:30:53 +0300

    fix: fix vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/codeestetic/',
   server: {
     port: 4000,
     host: true,
