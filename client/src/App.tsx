2946f34b7ff58316ff13f13613c7ff66a44c8ce3
Egor Gavrilenko <bambloo769@gmail.com>
2023-11-26 20:06:34 +0300

    feat: implement app

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './components/Header/Header'
+import Table from './components/Table/components/Table'
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Table />
+    </div>
+  )
+}
+
+export default App
